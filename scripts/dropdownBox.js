157c8ab11ecc41bdf38f3d0103bc5ee1698627bc
BikramGhart1 <coludingrain@gmail.com>
2024-10-13 18:17:11 +0545

    responsive update

---
+++ scripts/dropdownBox.js
@@ -0,0 +1,5 @@
+
+document.getElementById("dropdownButton").addEventListener("click", function () {
+    const dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.toggle("hiddenDrop"); // Toggle the hidden class
+});
